73bceee98a0f58474d98a5c4000955b352a56f3c
nikit <tnikitha57@gmail.com>
2022-05-05 15:19:49 +0530

    done

---
+++ app/src/main/java/com/example/project/UserHelperClass.java
@@ -0,0 +1,84 @@
+package com.example.project;
+
+public class UserHelperClass
+{
+    public String name,age,email,pwd,gender,height,weight,bloodgrp;
+    public UserHelperClass(){
+
+    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(String age) {
+//        this.age = age;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPwd() {
+//        return pwd;
+//    }
+//
+//    public void setPwd(String pwd) {
+//        this.pwd = pwd;
+//    }
+//
+//    public String getGender() {
+//        return gender;
+//    }
+//
+//    public void setGender(String gender) {
+//        this.gender = gender;
+//    }
+//
+//    public String getHeight() {
+//        return height;
+//    }
+//
+//    public void setHeight(String height) {
+//        this.height = height;
+//    }
+//
+//    public String getWeight() {
+//        return weight;
+//    }
+//
+//    public void setWeight(String weight) {
+//        this.weight = weight;
+//    }
+//
+//    public String getBloodgrp() {
+//        return bloodgrp;
+//    }
+//
+//    public void setBloodgrp(String bloodgrp) {
+//        this.bloodgrp = bloodgrp;
+//    }
+
+    public UserHelperClass(String name, String age, String email, String pwd, String gender, String height, String weight, String bloodgrp) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.pwd = pwd;
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight;
+        this.bloodgrp = bloodgrp;
+    }
+}
